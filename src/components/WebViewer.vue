3848d6fdb8f353a2b55cb43801045002f0861cd9
malachit-sekady <malachit@sekady.com>
2022-01-11 15:48:23 -0700

    Created example of PDF Tron canvas bug

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -1,43 +1,52 @@
 <template>
-  <div id="webviewer" ref="viewer"></div>
+  <div id="webviewer" ref="viewer" style="display: none"></div>
+  <!-- <div id="webviewer" ref="viewer"></div> -->
+  <div>sup {{previewHeight}}</div>
+  <input type="file" @change="handleFileChange"/>
+  <canvas
+    id="previewCanvas"
+    :width="previewWidth"
+    :height="previewHeight"
+  ></canvas>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+// import { ref, onMounted } from "vue";
 import WebViewer from "@pdftron/webviewer";
 
 export default {
   name: "WebViewer",
   props: { initialDoc: { type: String } },
-  setup(props) {
-    const viewer = ref(null);
-    onMounted(() => {
-      const path = `${process.env.BASE_URL}webviewer`;
-      WebViewer({ path, initialDoc: props.initialDoc }, viewer.value).then(
-        (instance) => {
-          const { documentViewer, annotationManager, Annotations } =
-            instance.Core;
-
-          documentViewer.addEventListener("documentLoaded", () => {
-            const rectangleAnnot = new Annotations.RectangleAnnotation({
-              PageNumber: 1,
-              // values are in page coordinates with (0, 0) in the top left
-              X: 100,
-              Y: 150,
-              Width: 200,
-              Height: 50,
-              Author: annotationManager.getCurrentUser(),
-            });
-            annotationManager.addAnnotation(rectangleAnnot);
-            annotationManager.redrawAnnotation(rectangleAnnot);
-          });
-        }
-      );
-    });
+  data() {
     return {
-      viewer,
-    };
+      webviewer: null,
+      previewWidth: 100,
+      previewHeight: 100
+    }
   },
+  async mounted() {
+    let path = `${process.env.BASE_URL}webviewer`;
+    this.webviewer = await WebViewer({ path, initialDoc: "https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf"}, document.getElementById('webviewer'));
+  },
+  methods: {
+    handleFileChange(event) {
+      console.log('viewer ', this.webviewer);
+      Array.from(event.target.files).map((javascriptFile) => {
+        this.webviewer.CoreControls.createDocument(javascriptFile).then((webviewerDoc) => {
+          webviewerDoc.loadCanvasAsync({
+            pageNumber: 1,
+            width: this.previewWidth,
+            height: this.previewHeight,
+            drawComplete: (loadedCanvas) => {
+              let canvasContext = document.getElementById('previewCanvas').getContext('2d');
+              canvasContext.clearRect(0, 0, this.previewWidth, this.previewHeight);
+              canvasContext.drawImage(loadedCanvas, 0, 0);
+            },
+          })
+        });
+      })
+    }
+  }
 };
 </script>
 
